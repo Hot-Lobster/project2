1b385569287db2645fc1427a3746c4db6b504939
olsoa4 <olsoa4@hotmail.com>
2018-10-26 19:34:32 -0500

    dsa

--- zombie-hunt/src/App.js
+++ zombie-hunt/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import Login from "./render/login.js";
 import Lobby from "./render/lobby.js";
-import UserName from "./render/login.js";
+import UserName from "./render/Username.js";
 import GameSet from "./render/gameSet.js";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import CSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
+import database from "./firebase/firebase";
 
 var FontAwesome = require("react-fontawesome");
 const theme = createMuiTheme({
@@ -32,7 +34,7 @@ class Game extends Component {
     super(props);
 
     this.state = {
-      stage: "UserName",
+      stage: "Login",
       showProgress: false,
       role: null,
       roomId: null,
@@ -40,6 +42,7 @@ class Game extends Component {
       toolbarTitle: ""
     };
 
+    this.handleLoginComplete = this.handleLoginComplete.bind(this);
     this.handleLobbyComplete = this.handleLobbyComplete.bind(this);
     this.handleUserNameComplete = this.handleUserNameComplete.bind(this);
     this.toggleIndeterminateProgressBar = this.toggleIndeterminateProgressBar.bind(
@@ -80,6 +83,22 @@ class Game extends Component {
           stage: "UserName"
         });
         break;
+      case "UserName":
+        // sign out first
+        this.toggleIndeterminateProgressBar();
+
+        database
+          .auth()
+          .signOut()
+          .then(() => {
+            this.setState({
+              toolbarTitle: "",
+              slideDirection: "slide-back", // slide backwards
+              stage: "login"
+            });
+            this.toggleIndeterminateProgressBar();
+          });
+        break;
     }
   }
 
@@ -95,8 +114,22 @@ class Game extends Component {
     });
   }
 
+  handleLoginComplete(createdNewUser) {
+    this.setState({
+      slideDirection: "slide", // forwards
+      stage: createdNewUser ? "UserName" : "lobby"
+    });
+  }
+
   getComponent() {
-    if (this.state.stage === "UserName") {
+    if (this.state.stage === "login") {
+      return (
+        <Login
+          toggleProgressBar={this.toggleIndeterminateProgressBar}
+          onComplete={this.handleLoginComplete}
+        />
+      );
+    } else if (this.state.stage === "UserName") {
       return (
         <UserName
           toggleProgressBar={this.toggleIndeterminateProgressBar}
--- zombie-hunt/src/render/login.js
+++ zombie-hunt/src/render/login.js
@@ -1,88 +1,186 @@
 import React, { Component } from "react";
-import database from "../firebase/firebase";
+import * as firebase from "firebase";
 
-export default class UserName extends Component {
+const INVALID_EMAIL = "Please enter a valid e-mail address.";
+const WRONG_PASSWORD = "Your password was incorrect.";
+const INVALID_PASSWORD = "Please include a minimum of 6 characters.";
+
+class Login extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      inputName: "", // @TEST
-      invalidName: false
+      inputEmail: "", // @TEST
+      inputPassword: "", // @TEST
+      invalidEmail: false,
+      invalidPassword: false,
+      passwordStatus: ""
     };
 
-    this.handleInputName = this.handleInputName.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputEmailChange = this.handleInputEmailChange.bind(this);
+    this.handleInputPasswordChange = this.handleInputPasswordChange.bind(this);
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
     this.updateUI = this.updateUI.bind(this);
+    this.createNewUserAndAuthenticate = this.createNewUserAndAuthenticate.bind(
+      this
+    );
   }
 
-  handleInputName(event) {
+  handleInputEmailChange(event) {
     this.setState({
-      inputName: event.target.value
+      inputEmail: event.target.value
     });
   }
 
-  handleSubmit(event) {
-    this.updateUI(0); // reset UI
-    event.preventDefault(); // don't refresh, etc
-
-    var inputName = this.state.inputName;
-
-    if (inputName === "") {
-      this.updateUI(1);
-      return;
-    }
-
-    this.props.toggleProgressBar(); // show progress bar
-
-    // Update Firebase with the input name
-    var db = database.database().ref();
-    db.child(database.auth().currentUser.uid)
-      .child("name")
-      .set(inputName)
-      .then(() => {
-        this.props.toggleProgressBar(); // hide progress bar
-        this.props.onComplete(); // complete this screen
-      });
+  handleInputPasswordChange(event) {
+    this.setState({
+      inputPassword: event.target.value
+    });
   }
 
   updateUI(status) {
     switch (status) {
       case 0: // reset
         this.setState({
-          invalidName: false
+          invalidEmail: false,
+          invalidPassword: false,
+          passwordStatus: ""
         });
         break;
-      case 1: // invalid name
+      case 1: // invalid email
         this.setState({
-          invalidRoomId: true
+          invalidEmail: true
+        });
+        break;
+      case 2: // weak password, need 6 chars
+        this.setState({
+          invalidPassword: true
+        });
+        break;
+      case 3: // wrong password
+        this.setState({
+          invalidPassword: true,
+          passwordStatus: "WRONG"
         });
         break;
     }
   }
 
+  handleLoginSubmit(event) {
+    this.props.toggleProgressBar(); // show progress bar to indicate loading status
+    this.updateUI(0);
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(
+        this.state.inputEmail,
+        this.state.inputPassword
+      )
+      .then(() => {
+        this.props.toggleProgressBar(); // done auth, hide progress bar
+        this.props.onComplete(false);
+      })
+      .catch(error => {
+        switch (error.code) {
+          case "auth/wrong-password":
+            this.updateUI(3);
+            this.props.toggleProgressBar();
+            break;
+          case "auth/invalid-email":
+            this.updateUI(1);
+            this.props.toggleProgressBar(); // hide progress bar, bad email
+            break;
+          case "auth/user-not-found":
+            this.createNewUserAndAuthenticate(
+              this.state.inputEmail,
+              this.state.inputPassword
+            );
+            break;
+        }
+      });
+  }
+
+  createNewUserAndAuthenticate(email, password) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        // Once account is created, log in
+        firebase
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+          .then(() => {
+            this.props.toggleProgressBar(); // done auth, hide progress bar
+            this.props.onComplete(true);
+          });
+      })
+      .catch(error => {
+        this.props.toggleProgressBar();
+
+        switch (error.code) {
+          case "auth/weak-password":
+            this.updateUI(2);
+            break;
+        }
+      });
+  }
+
+  componentDidMount() {
+    var unsub = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log("Authentication persisted");
+        this.props.onComplete(false);
+      } else {
+        console.log("Not authenticated");
+      }
+      unsub();
+    });
+  }
+
   render() {
+    var invalidEmail = this.state.invalidEmail ? "show" : "hidden";
+    var invalidPassword = this.state.invalidPassword ? "show" : "hidden";
+
     return (
-      <div id="chooseName" className="container">
-        <div className="wrapper center">
-          <form noValidate>
-            <input
-              className={this.state.invalidName ? "invalid" : ""}
-              placeholder="Choose a name"
-              id="inputName"
-              type="text"
-              value={this.state.inputName}
-              onChange={this.handleInputName}
-            />
-            <div onClick={this.handleSubmit} className="done-button">
-              <div className="arrow" />
-            </div>
-          </form>
+      <div id="login" className="container">
+        <form noValidate className="center">
+          <input
+            className={this.state.invalidEmail ? "invalid" : ""}
+            placeholder="E-mail Address"
+            id="inputEmail"
+            type="email"
+            value={this.state.inputEmail}
+            onChange={this.handleInputEmailChange}
+          />
+          <div className={invalidEmail + " invalid-email"}>
+            Please enter a valid e-mail address.
+          </div>
+
+          <input
+            className={this.state.invalidPassword ? "invalid" : ""}
+            placeholder="Password"
+            id="inputPassword"
+            type="password"
+            value={this.state.inputPassword}
+            onChange={this.handleInputPasswordChange}
+          />
+          <div className={invalidPassword + " invalid-password"}>
+            {this.state.passwordStatus === "WRONG"
+              ? WRONG_PASSWORD
+              : INVALID_PASSWORD}
+          </div>
+
+          <div onClick={this.handleLoginSubmit} className="login-button">
+            <div>SIGN IN</div>
+          </div>
 
           <div className="message">
-            Choose a name that you will be visible as.
+            You can use any e-mail, as long as you remember the password for it.
+            We won't ever send you anything.
           </div>
-        </div>
+        </form>
       </div>
     );
   }
 }
+export default Login;
